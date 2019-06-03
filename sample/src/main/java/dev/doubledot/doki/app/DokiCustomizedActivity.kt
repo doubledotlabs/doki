782d43347437f44cccd047c5a30fb779de979cec
James Fenn <18jafenn90@gmail.com>
2019-06-03 15:42:28 -0400

    simplify sample activities

--- sample/src/main/java/dev/doubledot/doki/app/DokiCustomizedActivity.kt
+++ sample/src/main/java/dev/doubledot/doki/app/DokiCustomizedActivity.kt
@@ -1,5 +1,21 @@
 package dev.doubledot.doki.app
 
-class DokiCustomizedActivity : DokiActivity() {
-    override val dokiLayout: Int = R.layout.activity_doki_custom
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.doubledot.doki.views.DokiContentView
+
+class DokiCustomizedActivity : AppCompatActivity() {
+
+    private val dokiContent: DokiContentView? by lazy {
+        findViewById<DokiContentView?>(R.id.doki_content)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doki_custom)
+
+        dokiContent?.setOnCloseListener { supportFinishAfterTransition() }
+        dokiContent?.loadContent()
+    }
+
 }
--- sample/src/main/java/dev/doubledot/doki/app/DokiThemedActivity.kt
+++ sample/src/main/java/dev/doubledot/doki/app/DokiThemedActivity.kt
@@ -1,3 +1,21 @@
 package dev.doubledot.doki.app
 
-class DokiThemedActivity : DokiActivity()
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.doubledot.doki.views.DokiContentView
+
+class DokiThemedActivity : AppCompatActivity() {
+
+    private val dokiContent: DokiContentView? by lazy {
+        findViewById<DokiContentView?>(R.id.doki_content)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doki)
+
+        dokiContent?.setOnCloseListener { supportFinishAfterTransition() }
+        dokiContent?.loadContent()
+    }
+
+}
